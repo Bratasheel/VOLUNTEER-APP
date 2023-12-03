3d09280fa170cf7662c3d92a539edbb1d13f85f1
Bratasheel <2028016@kiit.ac.in>
2023-12-04 00:29:37 +0530

    loginpage

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,39 +1,50 @@
-import { useState, useEffect } from 'react'
-import { createClient } from '@supabase/supabase-js'
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-import { supabase } from '../lib/supabase'
-import '../styles/layout.css'
-import Home from '../pages/homepage'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+import { supabase } from '../lib/supabase';
+import '../styles/layout.css';
+import Home from '../pages/homepage';
 
 export default function Layout() {
-  const [session, setSession] = useState(null)
+  const [session, setSession] = useState(null);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
+      setSession(session);
+    });
 
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
+      setSession(session);
+    });
 
-    return () => subscription.unsubscribe()
-  }, [])
+    return () => subscription.unsubscribe();
+  }, []);
 
-  if (!session) {
-    return (
-      <div className="layout-container">
-        <div className="login-box">
-          <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+  return (
+    <div className={`layout-container ${session ? 'logged-in' : ''}`}>
+      {!session ? (
+        <div className="login-popup">
+          <Auth 
+              supabaseClient={supabase}
+              appearance={{ theme: ThemeSupa }}
+              providers={['google', 'facebook', 'twitter']}
+              queryParams={{
+                access_type: 'offline',
+                prompt: 'consent',
+                hd: 'domain.com',
+              }}
+              
+              providerScopes={{
+                google: 'https://www.googleapis.com/auth/calendar.readonly',
+              }}
+          />
         </div>
-      </div>
-    )
-  } else {
-    return <div>
-      <Home/>
+      ) : (
+        <Home />
+      )}
     </div>
-  }
+  );
 }
