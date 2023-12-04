d6210a86c617c84c8408b47c407a2189de065711
AlphaBolt <2028014@kiit.ac.in>
2023-12-04 21:28:43 +0530

    Added accordion style cards in events and improved dropdown menu

--- src/components/Volunteers.jsx
+++ src/components/Volunteers.jsx
@@ -26,16 +26,18 @@ function VolunteerList() {
 
   return (
     <div>
-      <ul>
-        {volunteers.map((volunteer) => (
-          <li key={volunteer.volunteer_id}>
-            <p>{volunteer.first_name} {volunteer.last_name} :<br/>
-            Skills :{volunteer.skills} <br/>
-            Address :{volunteer.address}</p>
+      <div className="volunteer-list">
+          <ul className='volunteer-cards'>
+            {volunteers.map((volunteer) => (
+              <li key={volunteer.volunteer_id} className='volunteer-item'>
+                <p><svg xmlns="http://www.w3.org/2000/svg" height="25" width="25" viewBox="0 0 512 512"><path d="M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"/></svg>{volunteer.first_name} {volunteer.last_name} :<br/>
+                Skills :{volunteer.skills} <br/>
+                Address :{volunteer.address}</p>
 
-            </li>
-        ))}
-      </ul>
+                </li>
+            ))}
+          </ul>
+      </div>
     </div>
   );
 }
--- src/components/eventlists.jsx
+++ src/components/eventlists.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {supabaseConfig} from '../lib/supabase'
+import {supabaseConfig} from '../lib/supabase';
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
@@ -26,29 +26,41 @@ function EventList() {
   return (
     <div>
       <h2>Events List</h2>
-      
-      <div className="event-list">
+      <div class="row row-cols-1 row-cols-md-3 g-4">
 
-          <ul className='event-cards'>
-            {events.map((event) => (              
-                <li key={event.event_id} className='event-item'>
-                  {/* <div className='event-card' style={{backgroundImage: `url(images/${event.organizations.organization_name.replace(/\s+/g,'-')}1.jpg)`,}}> */}
-                  <div className='event-card'>
+      {events.map((event) => ( 
+        
+        <div class="col" key={event.event_id}>
+          <div class="card" >
+              <div class="card-body">
+                <div className="accordion accordion-flush" id={`accordionFlushExample-${event.event_id}`}>
+              <div className="accordion-item">
+                <h2 className="accordion-header">
+                  <button className="accordion-button collapsed text-white" type="button" data-bs-toggle="collapse" data-bs-target={`#flush-collapse-${event.event_id}`} aria-expanded="false" aria-controls={`flush-collapse-${event.event_id}`} 
+                  style={{ backgroundImage: `url(images/${event.organizations.organization_name.replace(/\s+/g, '-')}.jpg)`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat',}}>
                     <h3>{event.event_name}</h3>
+                  </button>
+                </h2>
+                <div id={`flush-collapse-${event.event_id}`} className="accordion-collapse collapse" data-bs-parent={`#accordionFlushExample-${event.event_id}`}>
+                  <div className="accordion-body">
                     <p>Associated Org : <strong>{event.organizations.organization_name}</strong><br/>
                     Desc: {event.description} <br/>
-                    
-                  Location: {event.location}</p>
-
+                    Location: {event.location}</p>
                   </div>
-                  </li>
-              
-            ))}
-          </ul>
+                </div>
+              </div>
+              {/* Add more accordion here */}
+            </div>
+              </div>
+            </div>
+        </div>
+      ))}
+</div>
 
-      </div>
     </div>
   );
+
+
 }
 
 export default EventList;
